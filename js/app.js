785febe369d4ee899959f0099403a6edae6148e1
Maddie <maddie.huebner@gmail.com>
2016-01-24 15:40:02 -0600

    Add markers to map!

--- js/app.js
+++ js/app.js
@@ -8,19 +8,47 @@ function initMap() {
         center: myLatLng
     });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
-    });
+    setMarkers(map);
 }
+
+
+// Adapted from:  https://developers.google.com/maps/documentation/javascript/examples/icon-complex
+
+// Data for the markers consisting of a name, a LatLng and a zIndex for the
+// order in which these markers should display on top of each other.
+
+// MODEL //
+var restaurants = [
+    ['Cafe Hollander', 43.0664868, -87.87782729999998, 4],
+    ['Beans & Barley', 43.05928, -87.892999, 5],
+    ['Colectivo', 43.0591269, -87.88515799999999, 3],
+    ['Rochambo', 43.052783, -87.894867, 2],
+    ['Anodyne', 43.0254467, -87.91364299999998, 1]
+];
+
+function setMarkers(map) {
+    // Adds markers to the map.
+    for (var i = 0; i < restaurants.length; i++) {
+        var restaurant = restaurants[i];
+        var marker = new google.maps.Marker({
+            position: {lat: restaurant[1], lng: restaurant[2]},
+            map: map,
+            title: restaurant[0],
+            zIndex: restaurant[3]
+        });
+    }
+}
+
+
+
+// ko.applyBindings(new RestaurantsViewModel());
 
 //Knockout js
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = ko.observable("Joe");
-    this.lastName = ko.observable("Bertington");
-}
-
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
+//function AppViewModel() {
+//    this.firstName = ko.observable("Joe");
+//    this.lastName = ko.observable("Bertington");
+//}
+//
+//// Activates knockout.js
+//ko.applyBindings(new AppViewModel());
