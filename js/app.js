dd9fb1e8c606f42107578f7a5e67e1ebad6983aa
Maddie <maddie.huebner@gmail.com>
2016-02-03 20:50:20 -0600

    Reformat model data, attempt to add map markers

--- js/app.js
+++ js/app.js
@@ -1,68 +1,132 @@
-// Map scripts
-
-function initMap() {
-    var myLatLng = {lat: 43.05, lng: -87.95};
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: myLatLng
-    });
-
-    setMarkers(map);
-}
-
 
 // Adapted from:  https://developers.google.com/maps/documentation/javascript/examples/icon-complex
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
 
+// Source of help with connecting data to map:  https://discussions.udacity.com/t/ive-hit-a-wall/43376/5
+
 // MODEL //
-var restaurants = [
+var initialLocations = [
     {
         name: "Cafe Hollander",
-        lat: 43.0664868,
-        lng: -87.87782729999998
+        latLng: (43.0664868, -87.87782729999998)
     },
     {
         name: "Beans & Barley",
-        lat: 43.05928,
-        lng: -87.892999
+        latLng: (43.05928, -87.892999)
     },
     {
         name: "Colectivo",
-        lat: 43.0591269,
-        lng: -87.88515799999999
+        latLng: (43.0591269, -87.88515799999999)
     },
     {
         name: "Rochambo",
-        lat: 43.052783,
-        lng: -87.894867
+        latLng: (43.052783, -87.894867)
     },
     {
         name: "Anodyne",
-        lat: 43.0254467,
-        lng: -87.91364299999998
+        latLng: (43.0254467, -87.91364299999998)
     }
 ];
 
-function setMarkers(map) {
-    // Adds markers to the map.
-    for (var i = 0; i < restaurants.length; i++) {
-        var restaurant = restaurants[i];
-        var marker = new google.maps.Marker({
-            position: {lat: restaurant[1], lng: restaurant[2]},
-            map: map,
-            title: restaurant[0],
-            zIndex: restaurant[3]
-        });
+
+
+
+var viewModel = function() {
+    var self = this;
+
+    this.locationList = ko.observableArray([]);
+
+    var Location = function(name, lat, lng) {
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
     }
+
+    //Display markers
+
+    for (var i = 0; i < initialLocations.length; i++) {
+        //var restaurant = restaurants[i];
+        self.locationList.push(new Location(initialLocations[i]));
+    };
+
+    for(var i=0; i< this.locationList.length; i++) {
+
+        var marker = new google.maps.Marker({
+            position: this.initialLocations[i](latLng),
+            map: map,
+            title: this.locationList[i].name
+
+            //position: {lat: restaurant[1], lng: restaurant[2]},
+            //map: map,
+            //title: restaurant[0],
+            //zIndex: restaurant[3]
+        });
+
+        //initialLocations.forEach(function(locationItem){
+        //    self.locationList.push( new Location(locationItem) );
+        //    }),
+
+        //this.currentLocation = ko.observable(this.locationList()[0]);
+
+        // Map scripts
+        function initMap() {
+            var myLatLng = {lat: 43.05, lng: -87.95};
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: myLatLng
+            });
+
+            viewModel();
+
+        }
+
+
+    }
+
+//
+//
+//function RestaurantsViewModel(name, lat, lng) {
+//    this.name = name;
+//    this.lat = ko.observable(lat);
+//    this.lng = ko.observable(lng);
+//
+//    var marker = new google.maps.Marker({
+//        position: {lat, lng},
+//        map: map,
+//        title: name
+//    })
+//}
+
 }
+ko.applyBindings(new viewModel());
+
+
+
+//
+//function setMarkers(map) {
+//    // Adds markers to the map.
+//
+//    for (var i = 0; i < restaurants.length; i++) {
+//        var restaurant = restaurants[i];
+//        var marker = new google.maps.Marker({
+//            position: {lat, lng},
+//            map: map,
+//            title: name
+//
+//            //position: {lat: restaurant[1], lng: restaurant[2]},
+//            //map: map,
+//            //title: restaurant[0],
+//            //zIndex: restaurant[3]
+//        });
+//    }
+//}
 
 
 
 
- ko.applyBindings(new RestaurantsViewModel());
 
 //Knockout js
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
