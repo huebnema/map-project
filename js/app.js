1b62780f26c1a0612c632a59a6c7d6ef21a31e35
Maddie <maddie.huebner@gmail.com>
2016-04-02 10:51:02 -0500

    Initial integration with Flickr

--- js/app.js
+++ js/app.js
@@ -37,7 +37,22 @@ var viewModel = function() {
         center: {lat: 43.05, lng: -87.95},
         zoom: 12
     });
-    
+
+    // Call to Flickr to load images
+        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        $.getJSON( flickerAPI, {
+                tags: "mount rainier",
+                tagmode: "any",
+                format: "json"
+            })
+            .done(function( data ) {
+                $.each( data.items, function( i, item ) {
+                    $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+                    if ( i === 3 ) {
+                        return false;
+                    }
+                });
+            });
 
 // Build "Place" objects to store the place data from locations
     self.allPlaces = [];
